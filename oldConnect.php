0f3522e8bef549d2410a69f30ece91c92ba8f3ce
mdln97 <madalinc.preda@gmail.com>
2019-10-31 14:59:36 +0000

    fixed connection strings

--- oldConnect.php
+++ oldConnect.php
@@ -1,27 +1,15 @@
 <?php
- $host = "mysql.cms.gre.ac.uk";
- $database = "mdb_cp3526m";
- $username = "cp3526m";
- $password = "Peer21";
+ $host = "localhost";
+ $database = "phpcw";
+ $username = "root";
+ $password = "";
 
 // Create connection
 $mysqli = new mysqli($host, $username, $password, $database);
 
-if (mysqli_connect_errno($conn)) {
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	exit();
+// Check connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
 }
-if ($result = mysqli_query($conn, "SELECT * FROM table")) {
-	printf("Select returned %d rows.\n", mysqli_num_rows($result));
-	echo "Result set order...";
-	$result->data_seek(0);
-	while ($row = $result->fetch_assoc()) {
-		echo $row['field1'].' '.$row['field2'];
-	}
-	mysqli_free_result($result);
-}
-
 echo "Connected successfully";
-
-mysqli_close($conn);
 ?>
